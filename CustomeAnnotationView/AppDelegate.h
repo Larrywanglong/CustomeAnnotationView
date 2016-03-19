a64eff0b5186592f93234d496c04631fd90ec85a
Larrywanglong <wanglong@wanglongdemac-mini.local>
2016-03-19 14:39:57 +0800

    Initial Commit

---
+++ CustomeAnnotationView/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  CustomeAnnotationView
+//
+//  Created by 王龙 on 16/3/19.
+//  Copyright © 2016年 Larry（Lawrence）. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ CustomeAnnotationView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CustomeAnnotationView
+//
+//  Created by 王龙 on 16/3/19.
+//  Copyright © 2016年 Larry（Lawrence）. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
